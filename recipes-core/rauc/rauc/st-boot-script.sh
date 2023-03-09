97814f9844b9a49bac2b6eab383f22e796d325d7
Christophe Guibout <christophe.guibout@st.com>
2023-03-09 17:19:48 +0100

    rauc: avoid rauc status to return errors
    Change-Id: Ic9b1de987f545c122a182aa138865c1a3dfb8d1b

--- recipes-core/rauc/rauc/st-boot-script.sh
+++ recipes-core/rauc/rauc/st-boot-script.sh
@@ -6,13 +6,13 @@ arg1=$1
 
 if [ "$arg1" = "get-primary" ]
 then
-    cat /proc/cmdline | grep "rauc.slot=A"
+    cat /proc/cmdline | grep "rauc.slot=A" > /dev/null
     if test $? == 0
     then
-        /usr/lib/fwu/update_metadata.sh 0 0 accept
+        /usr/lib/fwu/update_metadata.sh 0 0 accept > /dev/null
         echo "A"
     else
-        /usr/lib/fwu/update_metadata.sh 1 1 accept
+        /usr/lib/fwu/update_metadata.sh 1 1 accept > /dev/null
         echo "B"
     fi
     exit 0
